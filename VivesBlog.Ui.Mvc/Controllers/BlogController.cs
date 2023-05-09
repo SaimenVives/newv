b0a6319458ce5eeacc2de204a8269fb36dcb23f7
Nevix <126560882+SaimenVives@users.noreply.github.com>
2023-05-09 16:04:22 +0200

    Initial commit

---
+++ VivesBlog.Ui.Mvc/Controllers/BlogController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VivesBlog.Ui.Mvc.Core;
+using VivesBlog.Ui.Mvc.Models;
+
+namespace VivesBlog.Ui.Mvc.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly VivesBlogDbContext _dbContext;
+
+        public BlogController(VivesBlogDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            var articles = _dbContext.Articles.ToList();
+            return View(articles);
+        }
+        
+    }
+}
